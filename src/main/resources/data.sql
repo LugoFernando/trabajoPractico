9941761c23c23f826b4db7c009cc37e10a68f46c
selgadis <osmargastonescobar@gmail.com>
2024-09-17 15:53:27 -0300

    rama selgadis con carga de imagen

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
 INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
+INSERT INTO Figura(id, nombre, estado, descripcion, fotoUrl)
+VALUES(null, 'superman', 'Nuevo', 'Descripción de la figura', 'http://localhost:8080/images/dc.png
+');
