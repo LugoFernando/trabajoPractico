3bffcc47bd989fb0399a578139a0cc5f35488a11
Fernando <fer120793@gmail.com>
2024-09-17 23:30:13 -0300

    Eliminar figura funcional , correccion de direcciones , subir figura ( sin foto ) funcional.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
 INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
+INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'fer@gmail.com', '123456', 'ADMIN', true);
 
-INSERT INTO Figura(id, nombre, estado, descripcion, fotoUrl) VALUES(null, 'superman', 'Nuevo', 'Descripción de la figura', 'images/dc.png');
-INSERT INTO Figura(id, nombre, estado, descripcion, fotoUrl) VALUES(null, 'lufy', 'Nuevo', 'Descripción de la figura', 'images/anime.png');
-INSERT INTO Figura(id, nombre, estado, descripcion, fotoUrl) VALUES(null, 'iroman', 'Nuevo', 'Descripción de la figura', 'images/marvel.png');
+INSERT INTO Figura(id, nombre, precio , estado, descripcion, fotoUrl) VALUES(null, 'superman', 2000, 'Nuevo', 'Descripción de la figura', 'images/dc.png');
+INSERT INTO Figura(id, nombre, precio , estado, descripcion, fotoUrl) VALUES(null, 'lufy',2000, 'Nuevo', 'Descripción de la figura', 'images/anime.png');
+INSERT INTO Figura(id, nombre, precio , estado, descripcion, fotoUrl) VALUES(null, 'iroman', 2000, 'Nuevo', 'Descripción de la figura', 'images/marvel.png');
+INSERT INTO Figura(id, nombre, precio , estado, descripcion ) VALUES(null, 'ej', 2000, 'Nuevo', 'Descripción de la figura');
