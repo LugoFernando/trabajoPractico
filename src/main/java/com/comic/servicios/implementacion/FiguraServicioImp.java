c32b804e32a008e9eac837aa94dee59bcfb704df
Fernando <fer120793@gmail.com>
2024-09-19 00:29:25 -0300

    Funcion de logeo , lista y subir foto andando

--- src/main/java/com/comic/servicios/implementacion/FiguraServicioImp.java
+++ src/main/java/com/comic/servicios/implementacion/FiguraServicioImp.java
@@ -5,8 +5,11 @@ import com.comic.repositorios.FiguraRepositorio;
 import com.comic.servicios.FiguraServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 @Service("FiguraServicio")
@@ -27,9 +30,18 @@ public class FiguraServicioImp implements FiguraServicio {
     }
 
     @Override
-    public void guardarFigura(Figura figura) {
+    public void guardarFigura(Figura figura , MultipartFile imagen) {
+        if(imagen != null) {
+            try {
+                figura.setImagen(Base64.getEncoder().encode(imagen.getBytes()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         figuraRepositorio.guardar(figura);
     }
+
+
 
     @Override
     public Figura obtenerFiguraPorId(Long id) {
