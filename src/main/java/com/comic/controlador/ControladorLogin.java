5ce992239933e06cb60a63df8a928eac0baf7219
Fernando <fer120793@gmail.com>
2024-10-02 00:31:58 -0300

    eliminar duplicados

--- src/main/java/com/comic/controlador/ControladorLogin.java
+++ src/main/java/com/comic/controlador/ControladorLogin.java
@@ -1,16 +1,14 @@
 package com.comic.controlador;
 
 import com.comic.controlador.dto.DatosLogin;
-import com.comic.entidades.Figura;
+
 import com.comic.entidades.Preferencias;
-import com.comic.servicios.ServicioFigura;
 import com.comic.servicios.ServicioLogin;
 import com.comic.entidades.Usuario;
 import com.comic.dominio.excepcion.UsuarioExistente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
